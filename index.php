58cb5e882783e1c797d899a6cc28aec36106612c
Gabriel Mesquita <biel.mesquita23@gmail.com>
2017-07-13 00:47:16 -0300

    comit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="public/css/bootstrap.css">
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
