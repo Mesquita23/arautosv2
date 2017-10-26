09a4c66bb781b25cdc5d9d783636c276443b9056
Gabriel Mesquita <biel.mesquita23@gmail.com>
2017-10-26 15:54:43 -0200

    retorno
    agora vai

---
+++ carrocel.php
@@ -0,0 +1,34 @@
+                    <!-- Top part of the slider -->
+                    <div class="row">
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="public/img/carrossel/3.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="public/img/carrossel/1.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="public/img/carrossel/2.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  
+                    </div>
+                </div>
+            </div>
+            <!--/Slider-->
+        </div>
+
+    </div>
--- index.php
+++ index.php
@@ -124,7 +124,7 @@
     <header>
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in">Bem vindo ao Arautos!</div>
+                <div class="intro-lead-in">Bem vindo a Arautos!</div>
                 <div class="intro-heading">Prazer em vê-lo</div>
                 <a href="#services" class="page-scroll btn btn-xl">ARAUTOS</a>
             </div>
@@ -489,7 +489,7 @@
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Entre em contato</h2>
                     <h3 class="section-subheading text-muted">tire qualquer dúvida conosco</h3>
-					<h4 class="section-subheading text-muted">Caso o enviador de emails não esteja em funcionamento por motivos técninocs entre em contato com a gente pelo <strong>arautosdonarguile@gmail.com</strong></h4>
+					<h4 class="section-subheading text-muted">Caso o enviador de emails não esteja em funcionamento por motivos técnicos entre em contato com a gente pelo <strong>arautosdonarguile@gmail.com</strong></h4>
                 </div>
             </div>
             <div class="row">
@@ -794,76 +794,7 @@
             
             <div class="col-sm-6" style="margin: 0px auto; float: none;">
                 <div class="col-xs-12" id="slider">
-                    <!-- Top part of the slider -->
-                    <div class="row">
-                        <div class="col-sm-12" id="carousel-bounding-box">
-                            <div class="carousel slide" id="myCarousel">
-                                <!-- Carousel items -->
-                                <div class="carousel-inner">
-                                    <div class="active item" data-slide-number="0">
-                                        <img src="http://placehold.it/470x480&text=zero"></div>
 
-                                    <div class="item" data-slide-number="1">
-                                        <img src="http://placehold.it/470x480&text=Titi é gay"></div>
-
-                                    <div class="item" data-slide-number="2">
-                                        <img src="http://placehold.it/470x480&text=kbc é gay"></div>
-
-                                    <div class="item" data-slide-number="3">
-                                        <img src="http://placehold.it/470x480&text=Paulo é gay"></div>
-
-                                    <div class="item" data-slide-number="4">
-                                        <img src="http://placehold.it/470x480&text=Jojo é gay"></div>
-
-                                    <div class="item" data-slide-number="5">
-                                        <img src="http://placehold.it/470x480&text=Gabriel heterozão"></div>
-                                    
-                                    <div class="item" data-slide-number="6">
-                                        <img src="http://placehold.it/470x480&text=6"></div>
-                                    
-                                    <div class="item" data-slide-number="7">
-                                        <img src="http://placehold.it/470x480&text=7"></div>
-                                    
-                                    <div class="item" data-slide-number="8">
-                                        <img src="http://placehold.it/470x480&text=8"></div>
-                                    
-                                    <div class="item" data-slide-number="9">
-                                        <img src="http://placehold.it/470x480&text=9"></div>
-                                    
-                                    <div class="item" data-slide-number="10">
-                                        <img src="http://placehold.it/470x480&text=10"></div>
-                                    
-                                    <div class="item" data-slide-number="11">
-                                        <img src="http://placehold.it/470x480&text=11"></div>
-                                    
-                                    <div class="item" data-slide-number="12">
-                                        <img src="http://placehold.it/470x480&text=12"></div>
-
-                                    <div class="item" data-slide-number="13">
-                                        <img src="http://placehold.it/470x480&text=13"></div>
-
-                                    <div class="item" data-slide-number="14">
-                                        <img src="http://placehold.it/470x480&text=14"></div>
-
-                                    <div class="item" data-slide-number="15">
-                                        <img src="http://placehold.it/470x480&text=15"></div>
-                                </div>
-                                <!-- Carousel nav -->
-                                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                                    <span class="glyphicon glyphicon-chevron-left"></span>
-                                </a>
-                                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                                    <span class="glyphicon glyphicon-chevron-right"></span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--/Slider-->
-        </div>
-
-    </div>
 </div>
 
 
