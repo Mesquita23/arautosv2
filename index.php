c917179c4ea29a1068ac78e5219d806c59890568
Gabriel Mesquita <biel.mesquita23@gmail.com>
2017-10-30 00:29:56 -0200

    Views de login
    Apenas código html

--- index.php
+++ index.php
@@ -80,41 +80,12 @@
                     </li>   -->
                     <li>
                         <a class="page-scroll" href="#contact">Contato</a>
-                    </li>  
+                    </li>
+					<!--<li>
+                        <a class="page-scroll" href="login/login.html">Login</a>
+                    </li>  		-->			
 
-<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-			<ul id="login-dp" class="dropdown-menu">
-				<li>
-					 <div class="row">
-							<div class="col-md-12">
-								 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-										<div class="form-group">
-								<label class="sr-only" for="exampleInputEmail2">CPF:</label>
-				<input type="email" class="form-control" id="exampleInputEmail2" placeholder="CPF" required>
-										</div>
-										<div class="form-group">
-							<label class="sr-only" for="exampleInputPassword2">Password</label>
-					<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Senha" required>
-                                            
-										<div class="form-group">
-								 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-										</div>
-										<div class="checkbox">
-											 <label>
-											 <input type="checkbox"> keep me logged-in
-											 </label>
-										</div>
- 									
-								 </form>
-							</div>
-							<div class="bottom text-center">
-								New here ? <a href="#new"><b>Join Us</b></a>
-							</div>
-					 </div>
-				</li>
-                </ul>
-            </div>
+
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
@@ -787,6 +758,9 @@
                             <div class="modal-body">
                                 <!-- Project Details Go Here -->
                                 <h2>Narg na sua Casa</h2>
+								
+								
+								
 <div>
     <div id="main_area">
         <!-- Slider -->
@@ -811,6 +785,10 @@
             </div>
         </div>
     </div>
+	</div>
+	</div>
+	</div>
+	</div>
 
     <!-- Portfolio Modal 6 -->
     <div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
